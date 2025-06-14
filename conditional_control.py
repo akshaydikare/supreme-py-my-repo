f2fd1880da3b71da68a3bbffbdfb6d415578f103
akshaydikare <akshaydikare@gmail.com>
2025-06-14 21:25:44 +0530

    Explored Conditional Control Statements

---
+++ conditional_control.py
@@ -0,0 +1,11 @@
+#Lets practice conditional control statements in Python
+# This snippet demonstrates the use of conditional control statements in Python.
+
+# Initialize a variable
+x = 10
+# Check if x is greater than 5
+if x > 5:
+    print("x is greater than 5")
+# Check if x is less than 5
+elif x < 5:
+    print("x is less than 5")
