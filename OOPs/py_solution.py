668a965bc1689ae5b2775af56e3f69a65aaa7992
akshaydikare <akshaydikare@gmail.com>
2025-07-05 12:08:35 +0530

    OOPs - Class & Instances

---
+++ OOPs/py_solution.py
@@ -0,0 +1,11 @@
+class py_solution:
+    def sub_sets(self, sset):
+        return self.subsetsRecur([], sorted(sset))
+    def subsetsRecur(self, current, sset):
+        if sset:
+            return self.subsetsRecur(current, sset[1:]) + self.subsetsRecur(current + [sset[0]], sset[1:])
+        return [current]
+    def addTwoNumbers(a, b):
+        return a + b
+for name in py_solution.__dict__:
+    print(name)
